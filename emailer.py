59d9b54fde0c471d91958402c05b1518a65e8832
Ray Furtado <rayfurtado11@gmail.com>
2016-10-31 13:11:14 -0400

    emailer and nba web scrape

---
+++ emailer.py
@@ -0,0 +1,37 @@
+import smtplib 
+# import urllib.request
+from datetime import datetime
+# import bs4
+# import pandas as pd
+# from pandas import DataFrame
+
+
+
+# gmail
+# yahoo
+# outlook hotmail
+#login takes your email and password
+#send takes the recipient of email, subject, and the text
+class emailer:
+  def __init__(self, service):
+    service = service.lower()
+    if service == 'gmail':
+      service = 'smtp.gmail.com'
+    elif service == 'outlook' or service == 'hotmail':
+      service = 'smtp-mail.outlook.com'
+    elif service == 'yahoo':
+      service = 'smtp.mail.yahoo.com'
+    else:
+      raise NameError('Please enter, gmail, yahoo, outlook or hotmail') 
+    self.conn = smtplib.SMTP(service, 587)
+    self.conn.ehlo()
+    self.conn.starttls()
+
+  def login(self, email, pw):
+    self.email = email
+    self.conn.login(email, pw)
+
+  def send(self, recipient, subject, text):
+    self.conn.sendmail(self.email, recipient, subject, text)
+
+
---
+++ nba-stats.py
@@ -0,0 +1,48 @@
+import bs4 
+from datetime import datetime 
+import requests
+import pandas as pd 
+from pandas import DataFrame
+
+class nba:
+  def __init__(self, start_day, days):
+    self.start = str(start_day)
+    self.indexs = ['Name', '3PM', 'Rebounds', 'Assists', 'Steals', 'Blocks', 'Points', 'Fantasy_Points']
+    self.df = DataFrame(index = indexs)
+
+    def getData(self, minmum_mins):
+      counter = 0
+      while counter <= self.days:
+        self.url = "http://games.espn.com/fba/leaders?&scoringPeriodId="+self.start+"&seasonId=2017"
+        self.res = requests.get(self.url).text
+        self.soup = bs4.BeautifulSoup(self.res, 'html.parser')
+        table = self.soup.find(("table", {"class" : "playerTableTable tableBody"}))
+
+        for row in table.findAll('tr')[3:]:
+          col = row.findAll('td')
+          player_name = col[0].a.string.strip()
+          minutes = col[5].string.strip()
+          threes = col[10].string.strip()
+          rebounds = col[11].string.strip()
+          assists = col[12].string.strip()
+          steals = col[13].string.strip()
+          blocks = col[14].string.strip()
+          points = col[15].string.strip()
+
+          if minmum_mins:
+            if Minutes > minmum_mins:
+              stats = [player_name, threes, rebounds, assists, steals, blocks,point]
+              df_holder = DataFrame(stats, index =self.indexs)
+              self.df1 = self.df1.append(df2)
+          else:
+            stats = [player_name, threes, rebounds, assists, steals, blocks,point]
+            df_holder = DataFrame(stats, index =self.indexs)
+            self.df1 = self.df1.append(df2)
+        self.days += 1
+        counter += 1
+
+    def writeToCsv(self, name):
+      self.df1.to_csv(name, index=self.index)
+
+
+kobe = nba(1,1)
