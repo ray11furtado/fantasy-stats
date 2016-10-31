bee5dfef8f158e70e28b28a717913b61a1212889
Ray Furtado <rayfurtado11@gmail.com>
2016-10-31 13:22:13 -0400

    functioning, quick fixes

--- nba-stats.py
+++ nba-stats.py
@@ -7,7 +7,8 @@ from pandas import DataFrame
 class nba:
   def __init__(self, start_day, days):
     self.start = str(start_day)
-    self.indexs = ['Name', '3PM', 'Rebounds', 'Assists', 'Steals', 'Blocks', 'Points', 'Fantasy_Points']
+    self.days = days
+    self.indexs = ['Name', '3PM', 'Rebounds', 'Assists', 'Steals', 'Blocks', 'Points']
     self.df = DataFrame(index = self.indexs)
 
   def getData(self, minmum_mins):
@@ -15,7 +16,7 @@ class nba:
     while counter <= self.days:
       self.url = "http://games.espn.com/fba/leaders?&scoringPeriodId="+self.start+"&seasonId=2017"
       self.res = requests.get(self.url).text
-      self.soup = bs4.BeautifulSoup(self.res, 'html.parser')
+      self.soup = bs4.BeautifulSoup(self.res, 'lxml')
       table = self.soup.find(("table", {"class" : "playerTableTable tableBody"}))
 
       for row in table.findAll('tr')[3:]:
@@ -28,21 +29,26 @@ class nba:
         steals = col[13].string.strip()
         blocks = col[14].string.strip()
         points = col[15].string.strip()
+        print player_name
 
         if minmum_mins:
-          if Minutes > minmum_mins:
-            stats = [player_name, threes, rebounds, assists, steals, blocks,point]
+          if minutes > minmum_mins:
+            stats = [player_name, threes, rebounds, assists, steals, blocks, points]
             df_holder = DataFrame(stats, index =self.indexs)
-            self.df1 = self.df1.append(df2)
+            self.df = self.df.append(df_holder)
         else:
-          stats = [player_name, threes, rebounds, assists, steals, blocks,point]
+          stats = [player_name, threes, rebounds, assists, steals, blocks, points]
           df_holder = DataFrame(stats, index =self.indexs)
-          self.df1 = self.df1.append(df2)
-      self.days += 1
+          self.df = self.df.append(df_holder)
+      next_day = int(self.start) + 1
+      self.start = str(next_day)
       counter += 1
 
   def writeToCsv(self, name):
     name = name + '.csv'
-    self.df1.to_csv(name, index=self.indexs)
+    self.df.to_csv(name, index=self.indexs)
 
 
+kobe = nba(1,1)
+kobe.getData(20)
+kobe.writeToCsv('text')
