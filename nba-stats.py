3da51c6dd119c47db0e4052439b7e25daea198de
Ray Furtado <rayfurtado11@gmail.com>
2016-10-31 13:14:46 -0400

    tab error

--- nba-stats.py
+++ nba-stats.py
@@ -8,41 +8,41 @@ class nba:
   def __init__(self, start_day, days):
     self.start = str(start_day)
     self.indexs = ['Name', '3PM', 'Rebounds', 'Assists', 'Steals', 'Blocks', 'Points', 'Fantasy_Points']
-    self.df = DataFrame(index = indexs)
+    self.df = DataFrame(index = self.indexs)
 
-    def getData(self, minmum_mins):
-      counter = 0
-      while counter <= self.days:
-        self.url = "http://games.espn.com/fba/leaders?&scoringPeriodId="+self.start+"&seasonId=2017"
-        self.res = requests.get(self.url).text
-        self.soup = bs4.BeautifulSoup(self.res, 'html.parser')
-        table = self.soup.find(("table", {"class" : "playerTableTable tableBody"}))
+  def getData(self, minmum_mins):
+    counter = 0
+    while counter <= self.days:
+      self.url = "http://games.espn.com/fba/leaders?&scoringPeriodId="+self.start+"&seasonId=2017"
+      self.res = requests.get(self.url).text
+      self.soup = bs4.BeautifulSoup(self.res, 'html.parser')
+      table = self.soup.find(("table", {"class" : "playerTableTable tableBody"}))
 
-        for row in table.findAll('tr')[3:]:
-          col = row.findAll('td')
-          player_name = col[0].a.string.strip()
-          minutes = col[5].string.strip()
-          threes = col[10].string.strip()
-          rebounds = col[11].string.strip()
-          assists = col[12].string.strip()
-          steals = col[13].string.strip()
-          blocks = col[14].string.strip()
-          points = col[15].string.strip()
+      for row in table.findAll('tr')[3:]:
+        col = row.findAll('td')
+        player_name = col[0].a.string.strip()
+        minutes = col[5].string.strip()
+        threes = col[10].string.strip()
+        rebounds = col[11].string.strip()
+        assists = col[12].string.strip()
+        steals = col[13].string.strip()
+        blocks = col[14].string.strip()
+        points = col[15].string.strip()
 
-          if minmum_mins:
-            if Minutes > minmum_mins:
-              stats = [player_name, threes, rebounds, assists, steals, blocks,point]
-              df_holder = DataFrame(stats, index =self.indexs)
-              self.df1 = self.df1.append(df2)
-          else:
+        if minmum_mins:
+          if Minutes > minmum_mins:
             stats = [player_name, threes, rebounds, assists, steals, blocks,point]
             df_holder = DataFrame(stats, index =self.indexs)
             self.df1 = self.df1.append(df2)
-        self.days += 1
-        counter += 1
+        else:
+          stats = [player_name, threes, rebounds, assists, steals, blocks,point]
+          df_holder = DataFrame(stats, index =self.indexs)
+          self.df1 = self.df1.append(df2)
+      self.days += 1
+      counter += 1
 
-    def writeToCsv(self, name):
-      self.df1.to_csv(name, index=self.index)
+  def writeToCsv(self, name):
+    name = name + '.csv'
+    self.df1.to_csv(name, index=self.indexs)
 
 
-kobe = nba(1,1)
